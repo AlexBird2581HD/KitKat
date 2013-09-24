b1f3de7d539c238b1d884a4b10d95ea2eb7121f4
Matias Kangasjärvelä <matias.kangasjarvela@gmail.com>
2013-09-24 13:28:53 +0300

    Remade operators for fector class

--- Win32Build/Mathlibrary.cpp
+++ Win32Build/Mathlibrary.cpp
@@ -5,143 +5,149 @@
 namespace KitKat
 {
 
-	Vector::Vector()
-                         : x(0), y(0)
-            {
-	
-			}
-            Vector::Vector(float X, float Y)
-                         : x(X), y(Y)
-            {
-			
-			}
-            Vector::~Vector()
-			{
-			
-			}
+Vector2::Vector2()
+: x(0), y(0)
+{	
+}
+
+Vector2::Vector2(float X, float Y)
+	: x(X), y(Y)
+{			
+}
+
+Vector2::~Vector2(){}
  
-            float Vector::getLength()
-			{           
-				return sqrt(pow(x,2) + pow(y,2));
-		    }
-            float Vector::getAngle()
-            {
-				if (x == 0)
-					{
-						if (y > 0)
-							return 90;
-                        return 270;
-                    }
-                         else
-                    {
-							float angle = float(atan(y/x)*(180.0f/PI));
+float Vector2::getLength()
+{           
+	return sqrt(pow(x,2) + pow(y,2));
+}
+
+float Vector2::getAngle()
+{
+	if (x == 0)
+	{
+		if (y > 0)
+			return 90;
+		return 270;
+    }
+	else
+    {
+		float angle = float(atan(y/x)*(180.0f/PI));
  
-                            if (x < 0)
-								angle += 180;
+		if (x < 0)
+			angle += 180;
  
-                            else if (angle < 0)
-								angle += 360;
+		else if (angle < 0)
+			angle += 360;
  
-                            return angle;
-                   }
-            }
+		return angle;
+    }
+}
  
-            Vector operator -(const Vector& RightVal)
-            {
-				return Vector(-RightVal.x, -RightVal.y);
-            }
- 
-            Vector operator +(const Vector& LeftVal,const Vector& RightVal)
-            {
-				return Vector(LeftVal.x+RightVal.x,LeftVal.y+RightVal.y);
-            }
-            Vector operator -(const Vector& LeftVal,const Vector& RightVal)
-            {
-				return Vector(LeftVal.x-RightVal.x,LeftVal.y-RightVal.y);
-            }
- 
-            Vector operator +=(Vector& LeftVal,const Vector& RightVal)
-            {
-				LeftVal.x += RightVal.x;
-				LeftVal.y += RightVal.y;
-				return Vector(LeftVal.x,LeftVal.y);
-            }
-            Vector operator -=(Vector& LeftVal,const Vector& RightVal)
-            {
-				LeftVal.x -= RightVal.x;
-				LeftVal.y -= RightVal.y;
-				return Vector(LeftVal.x,LeftVal.y);
-            }
- 
-            Vector operator /(const Vector& LeftVal, const float& RightVal)
-            {
-				return Vector(LeftVal.x / (float)RightVal,LeftVal.y / (float)RightVal);
-            }
-            Vector operator /=(Vector& LeftVal, const float& RightVal)
-            {
-				LeftVal.x /= (float)RightVal;
-				LeftVal.y /= (float)RightVal;
-				return Vector(LeftVal.x,LeftVal.y);;
-            }
- 
-            bool operator ==(const Vector& LeftVal,const Vector& RightVal)
-            {
-				if (LeftVal.x == RightVal.x && LeftVal.y == RightVal.y)
-					return true;
-				return false;
-            }
-            bool operator !=(const Vector& LeftVal,const Vector& RightVal)
-            {
-				if (LeftVal.x == RightVal.x && LeftVal.y == RightVal.y)
-					return false;
-				return true;
-            }
- 
-            Vector operator *(const Vector& LeftVal, const float& RightVal)
-            {
-				return Vector(LeftVal.x * RightVal,LeftVal.y * RightVal);
-            }
-            Vector operator *(const float& LeftVal, const Vector& RightVal)
-            {
-				return Vector(LeftVal * RightVal.x,LeftVal * RightVal.y);
-            }
- 
-            const Vector operator *=(Vector& LeftVal,const float& RightVal)
-            {
-				LeftVal.x *= RightVal;
-				LeftVal.y *= RightVal;
-				return Vector(LeftVal.x,LeftVal.y);;
-            }
-				Rectangle::Rectangle()
-            {
-				left = 0;
-				top = 0;
-				width = 0;
-				height = 0;
-            }
-            Rectangle::Rectangle(float Left, float Top, float Width, float Height)
-            {
-				left = Left;
-				top = Top;
-				width = Width;
-				height = Height;
-            }
-            Rectangle::Rectangle(Vector Position, float Width, float Height)
-            {
-				left = Position.x;
-				top = Position.y;
-				width = Width;
-				height = Height;
-            }
-            Rectangle::Rectangle(Vector Position, Vector Size)
-            {
-				left = Position.x;
-				top = Position.y;
-				width = Size.x;
-				height = Size.y;
-            }
+
+Rectangle::Rectangle()
+{
+	left = 0;
+	top = 0;
+	width = 0;
+	height = 0;
+}
+Rectangle::Rectangle(float Left, float Top, float Width, float Height)
+{
+	left = Left;
+	top = Top;
+	width = Width;
+	height = Height;
+}
+Rectangle::Rectangle(Vector2 Position, float Width, float Height)
+{
+	left = Position.x;
+	top = Position.y;
+	width = Width;
+	height = Height;
+}
+Rectangle::Rectangle(Vector2 Position, Vector2 Size)
+{
+	left = Position.x;
+	top = Position.y;
+	width = Size.x;
+	height = Size.y;
+}
                          
-            Rectangle::~Rectangle()
-            {
-			}
+Rectangle::~Rectangle()
+{
+}
+
+
+// Operators - vector
+
+Vector2& Vector2::operator +=(const Vector2& RightVal)
+{
+	this->x += RightVal.x;
+	this->y += RightVal.y;
+	return *this;
+}
+Vector2& Vector2::operator -=(const Vector2& RightVal)
+{
+	this->x -= RightVal.x;
+	this->y -= RightVal.y;
+	return *this;
+}
+
+Vector2& Vector2::operator *=(const float& RightVal)
+{
+	this->x *= RightVal;
+	this->x *= RightVal;
+	return *this;
+}
+
+Vector2& Vector2::operator /=(const float& RightVal)
+{
+	this->x /= RightVal;
+	this->y /= RightVal;
+	return *this;
+}
+
+Vector2 Vector2::operator +(const Vector2& RightVal)
+{
+	return Vector2(this->x + RightVal.x, this->y + RightVal.y);
+}
+
+Vector2 Vector2::operator -(const Vector2& RightVal)
+{
+	return Vector2(this->x - RightVal.x, this->y - RightVal.y);
+}
+
+Vector2 Vector2::operator -()
+{
+	return Vector2(-this->x, -this->y);
+}
+
+Vector2 Vector2::operator *(const float& RightVal)
+{
+	return Vector2(this->x * RightVal, this->y * RightVal);
+}
+
+Vector2 Vector2::operator /(const float& RightVal)
+{
+	return Vector2(this->x / RightVal, this->y / RightVal);
+}
+
+Vector2 operator *(const float& LeftVal, const Vector2& RightVal)
+{
+	return Vector2(LeftVal * RightVal.x, LeftVal * RightVal.y);
+}
+
+bool Vector2::operator ==(const Vector2& RightVal)
+{
+	if (this->x == RightVal.x && this->y == RightVal.y)
+		return true;
+	return false;
+}
+
+bool Vector2::operator !=(const Vector2& RightVal)
+{
+	return !(*this == RightVal);
+}
+
 }
--- Win32Build/Mathlibrary.h
+++ Win32Build/Mathlibrary.h
@@ -3,60 +3,55 @@
 
 namespace KitKat
 {
-	class Vector
-	{
-	public:
-		Vector();
-		Vector(float X, float Y);
-		~Vector();
+class Vector2
+{
+public:
+	Vector2();
+	Vector2(float X, float Y);
+	~Vector2();
 
-		float getLength();
-		float getAngle();
-		float x,y;
-	private:
-	
-	};
+	float getLength();
+	float getAngle();
+	float x,y;
 
-			Vector operator -(const Vector& RightVal);
+	Vector2& operator +=(const Vector2& RightVal);
+    Vector2& operator -=(const Vector2& RightVal);
+
+	Vector2& operator *=(const float& RightVal);
+	Vector2& operator /=(const float& RightVal);
+
+    Vector2 operator +(const Vector2& RightVal);
+    Vector2 operator -(const Vector2& RightVal);
+	Vector2 operator -(); 
  
-            Vector operator +(const Vector& LeftVal,const Vector& RightVal);
-            Vector operator -(const Vector& LeftVal,const Vector& RightVal);
- 
-            Vector operator +=(Vector& LeftVal,const Vector& RightVal);
-            Vector operator -=(Vector& LeftVal,const Vector& RightVal);
- 
-            Vector operator /(const Vector& LeftVal,const float& RightVal);
-            Vector operator /=(Vector& LeftVal,const float& RightVal);
- 
-            bool operator ==(const Vector& LeftVal,const Vector& RightVal);
-            bool operator !=(const Vector& LeftVal,const Vector& RightVal);
- 
-            Vector operator *(const Vector& LeftVal, const float& RightVal);
-            Vector operator *(const float& LeftVal, const Vector& RightVal);
- 
-            const Vector operator *=(Vector& LeftVal, const float& RightVal);
+	Vector2 operator *(const float& RightVal);
+	Vector2 operator /(const float& RightVal);
+	// Needs leftVal
+    friend Vector2 operator *(const float& LeftVal, const Vector2& RightVal);
+
+    bool operator ==(const Vector2& RightVal);
+    bool operator !=(const Vector2& RightVal);
+};
 
 class Rectangle
 {
-	public:
+public:
 
-		Rectangle();
-		Rectangle(float Left, float Top, float Width, float Height);
-		Rectangle(Vector Position, float Width, float Height);
-		Rectangle(Vector Position, Vector Size);
+	Rectangle();
+	Rectangle(float Left, float Top, float Width, float Height);
+	Rectangle(Vector2 Position, float Width, float Height);
+	Rectangle(Vector2 Position, Vector2 Size);
 		
 		
-		bool intersects(Rectangle rectangle);
-		bool contains(Vector Position);
-		~Rectangle();
-		float width, height, left, top;
+	bool Intersects(Rectangle rectangle);
+	bool Contains(Vector2 Position);
+	~Rectangle();
+	float width, height, left, top;
 
-	private:
+private:
 
 
 };
-
-
 }
 
 #endif
