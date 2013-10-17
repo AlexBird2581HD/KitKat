bb6f64e1b92c8be7cd9b9b90532ec29a0f4c77ba
Iconum <TTI12sIidaK@kajak.fi>
2013-10-17 12:20:53 +0300

    -

--- Common/include/Shader.h
+++ Common/include/Shader.h
@@ -20,21 +20,14 @@ namespace KitKat
 
 		GLuint program();
 
-		GLint getAttribLocation(const std::string& name);
-		GLint getUniformLocation(const std::string& name);
-		// void setUniform(const std::string& name, matrix4);
-		//TODO: implement matrix4
-
 		void use();
 
 	private:
 		GLuint _program;
 		GLuint _vertexShader, _fragmentShader;
 
-		std::string readFile(const std::string fileName);
-
 		void create(const std::string& vertexCode, const std::string& fragmentCode);
-		void destroy();
+		void destroy();sy
 
 		// Creates the OpenGL program
 		void createProgram();
@@ -45,6 +38,6 @@ namespace KitKat
 		void compileShader(const GLuint shader, const char* code);
 		void attachShader(const GLuint shader);
 	};
-}
+}sf
 
 #endif
